1ebab6dd6010d8b7cd01593b81b1bce1ddc3f888
Zartab Nakhwa <nzartab@gmail.com>
2021-07-02 16:41:11 +0530

    Error And Completion Execution

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,17 +17,31 @@ export class HomeComponent implements OnInit,OnDestroy {
     //  console.log(count)
     // });
 
+    
     const customObservable=Observable.create(observer=>{
       let count=0;
       setInterval(()=>{
         observer.next(count);
+        if(count>3){
+          observer.complete()
+        }
+        if(count>5){
+          observer.error(new Error('Count is greater than 5!!'))
+        }
         count++
       },1000)
     })
 
     this.mySubscription=customObservable
-    .subscribe((data:number)=>{
+    .subscribe(
+    (data:number)=>{
       console.log(data)
+    },
+    (error)=>{
+      alert(error.message)
+    },
+    ()=>{
+      console.log('Observable completed')
     })
 
   }
